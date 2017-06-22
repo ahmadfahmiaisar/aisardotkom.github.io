4dc6eaa21ef14df1a82f32c564320f9fb50331fc
ahmad fahmi aisar <fahmiaisar99@gmail.com>
2017-06-22 11:47:26 +0700

    Add files via upload

---
+++ UAS/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "Bunglon mempunyai ciri khusus lidahnya panjang dan lengket. Ciri khusus khusus tersebut berfungsi untuk ...",
+	"option1": "a. menyesuaikan diri",
+	"option2": "B. menangkap mangsa",
+	"option3": "c. melindungi diri",
+	"option4": "d. menghindari musuh",
+	"answer": "2"
+}, {
+	"question": "Ulat sutra dapat menghasilkan serat yang berfungsi sebagai bahan.....",
+	"option1": "a. tas",
+	"option2": "b. tekstil",
+	"option3": "c. makanan",
+	"option4": "d. perhiasan",
+	"answer": "2"
+}, {
+	"question": "Pohon Cendana termasuk tumbuhan yang dilindungi. Tujuan perlindungan tumbuhan tersebut adalah....",
+	"option1": "a. menjadikan maskot daerah",
+	"option2": "b. melindungi dari kepunahan",
+	"option3": "c. mendatangkan wisatawan",
+	"option4": "d. untuk komoditi ekspor",
+	"answer": "2"
+}, {
+	"question": "Tahapan nimfa merupakan daur hidup .....",
+	"option1": "a. nyamuk",
+	"option2": "b. lalat",
+	"option3": "c. katak",
+	"option4": "d. kecoak",
+	"answer": "4"
+}, {
+	"question": "Dasar perhitungan kalender Komariah adalah ....",
+	"option1": "a. rotasi bumi",
+	"option2": "b. revolusi bumi",
+	"option3": "c. rotasi bulan",
+	"option4": "d. revolusi bulan",
+	"answer": "4"
+}, {
+	"question": "Revolusi Bumi menyebabkan terjadinya....",
+	"option1": "a. pergantian musim",
+	"option2": "b. angin darat dan angin laut",
+	"option3": "c. gerak semu harian matahari",
+	"option4": "d. siang dan malam",
+	"answer": "1"
+}, {
+	"question": "Salah satu sumber daya laut yang dapat dimanfaatkan untuk perhiasan adalah.....",
+	"option1": "a. batu karang",
+	"option2": "b. kerang laut",
+	"option3": "c. mutiara",
+	"option4": "d. rumput laut",
+	"answer": "3"
+}, {
+	"question": "Zat karbohidrat yang terkandung dalam makanan bagi tubuh berfungsi untuk ...",
+	"option1": "a. sumber tenaga dan cadangan makanan",
+	"option2": "b. pertumbuhan tulang dan gigi",
+	"option3": "c. pengatur suhu tubuh",
+	"option4": "d. pertumbuhan dan pengganti sel yang rusak",
+	"answer": "1"
+}, {
+	"question": "Ciri fisik yang dialami laki-laki pada masa pubertas adalah...",
+	"option1": "a. tumbuh payudara",
+	"option2": "b. pinggul membesar",
+	"option3": "c. dada menjadi bidang",
+	"option4": "d. suara menjadi tinggi",
+	"answer": "3"
+}, {
+	"question": "Kabel listrik yang ada di pinggir jalan sengaja dipasang kendur. Hal ini bertujuan agar...",
+	"option1": "a. tidak putus waktu memuai siang hari",
+	"option2": "b. tidak putus waktu menyusut malam hari",
+	"option3": "c. tidak putus waktu arus listrik mengalir",
+	"option4": "d. tidak putus waktu tertiup angin",
+	"answer": "2"
+}];
